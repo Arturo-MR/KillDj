67670b939e8468abea6d43492021a1c922d6c8b0
Arturo-MR <arturo10.mr@gmail.com>
2014-03-10 12:40:33 -0600

    Add new button in menu
    button "Change"

--- menu.php
+++ menu.php
@@ -2,6 +2,7 @@
 <ul>
 	<li><a href="index.php?type=new">New</a></li>
 	<li><a href="consult.php">Consult</a></li>
+	<li><a href="change.php">Change</a></li>
 	<li><a href="delete.php">Delete</a></li>
 
 </ul>
