2773da675379d5504a1f727d8cc2f5d990f1920f
Arturo-MR <arturo10.mr@gmail.com>
2014-03-10 13:33:41 -0600

    Class #2
    New file config.php connection DB
    Change alta.php (connection DB)
    Little changes in styles.css

--- alta.php
+++ alta.php
@@ -1,3 +1,4 @@
+<?php include "config.php"; ?>
 <link rel="stylesheet" href="css/styles.css">
 <form action="" method="post" enctype="multipart/form-data">
 <div class="tabla">
@@ -9,10 +10,27 @@
        <label>Album</label>
 		<input name="album" type="text" id="album" size="25" maxlength="100" required></p>
 	  <p>
-      <label>Year of Release</label>
-    	<input type = "text" name = "release" size = "25" maxlength = "4" required pattern="[0-9]{4}" title="Just 4 Numbers"></p>
+      	<label>Year of Release</label>
+		<select name="year" id="year">
+			<?php 
+
+
+				for($i=2014;$i>=1930;$i--){
+			?>
+				<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+				<?php } ?>
+		</select>
+    </p>
 			<p><label>Género(s)</label></p>
 			<p><label>País</label></p>
+			<select name="pais" id="pais">
+				<?php 
+					$sql = mysql_query("SELECT * FROM paises");
+					while($row = mysql_fetch_object($sql)){
+				?>
+				<option value="<?php echo $row->pais; ?>"><?php echo utf8_encode($row->pais); ?></option>
+				<?php } ?>
+			</select>
 			<p><label>Precio</label></p>
             <p>
       <label>Album Artwork</label>
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	mysql_connect("localhost","root","")
+	or die ("NOT CONNECTION");
+	mysql_select_db("killdj")
+	or die ("NOT EXIST DB");
+
+?>
