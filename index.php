2aa9183a492c98f9e475fd9d4769137b6aee1524
Arturo-MR <arturo10.mr@gmail.com>
2014-03-19 23:55:47 -0600

    Header

--- index.php
+++ index.php
@@ -1,25 +1,54 @@
 <!DOCTYPE html>
+<?php
+	include("config.php");
+?>
 <html lang="en">
 <head>	
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<title>KillDJ - Music for Real People</title>
+	<link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/styles.css">
     <link rel="icon" type="image/png" href="images/favicon5.png" />
     
 </head>
 <body>
-	<header>
-		<div id="logo"><a href="index.php"><img src="images/logo.png" fixed/></a></div>
-	</header>
-	<?php include "menu.php"; 
-		?>
-	<section>
-		<?php
-		if(@$_GET['type'] == 'new');
-			include "alta.php"; ?>
+	<header class="header relative border-bottom">
+		<a href="" class="button-menu icon-menu"></a>
+		<nav class="menu">
+			<ul class="list">
+				<li class="item">
+					<a href="#">Top Hits</a>
+				</li>
+				<li class="item">
+					<a href="#">Playlist</a>
+				</li>
+				<li class="item">
+					<a href="#">Top Hits</a>
+				</li>
+				<li class="item">
+					<a href="#">Top Hits</a>
+				</li>
+				<li class="item">
+					<a href="#">Top Hits</a>
+				</li>
+			</ul>
+		</nav>
+		<div class="title">
+			<span>
+				Home
+			</span>
+		</div>
+	<section class="container">
+		<article class="band">
 			
-
+		</article>
 	</section>
+
+	
+
+
+	
 </body>
 <script src = "MyScript.js"></script>
 </html>
